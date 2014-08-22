5ef880309aa9386300058eb1be00e137e86b7d86
Pyry-Samuli Lahti <pyry-samuli.lahti@onomatics.com>
2014-08-22 19:49:46 +0300

    Updated IrcBot

--- src/main/scala/fi/pyppe/estools/IrcBot.scala
+++ src/main/scala/fi/pyppe/estools/IrcBot.scala
@@ -5,27 +5,14 @@ import play.api.libs.json.{JsObject, Json, JsValue}
 /** For personal use by pyppe */
 object IrcBot {
 
-  val UrlRegex = ("(?i)\\b(((ht|f)tp(s?)\\:\\/\\/|~\\/|\\/)|www.)" +
-    "(\\w+:\\w+@)?(([-\\w]+\\.)+(com|org|net|gov" +
-    "|mil|biz|info|mobi|name|aero|jobs|museum" +
-    "|travel|[a-z]{2}))(:[\\d]{1,5})?" +
-    "(((\\/([-\\w~!$+|.,=]|%[a-f\\d]{2})+)+|\\/)+|\\?|#)?" +
-    "((\\?([-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?" +
-    "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)" +
-    "(&(?:[-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?" +
-    "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)*)*" +
-    "(#([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)?\\b").r
-
   def parseUrls(text: String): List[String] =
-    UrlRegex.findAllMatchIn(text).map(_.group(0)).toList
+    text.split("\\s+").map(_.trim).filter(_.matches("(?i)^(ftp|https?)://.+")).
+      map(_.replaceAll("(.*)[,!.:?()<>]$", "$1")).toList
 
   def modifyLinks(msg: JsValue): JsValue = {
-    (msg \ "linkCount").as[Int] match {
-      case 0 => msg
-      case _ =>
-        val links = parseUrls((msg \ "text").as[String])
-        (msg.as[JsObject]) ++ Json.obj("links" -> links, "linkCount" -> links.size)
-    }
+    val text = (msg \ "text").as[String]
+    val links = parseUrls(text)
+    msg.as[JsObject] ++ Json.obj("links" -> links, "linkCount" -> links.size)
   }
 
 }
